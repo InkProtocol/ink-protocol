305ccc6487cfe78aa8153421267e1973c50b5814
Jerry Chen <jerry@listia.com>
2018-01-30 12:18:37 +0800

    init

--- test/confirmTransaction.js
+++ test/confirmTransaction.js
@@ -2,10 +2,11 @@ const $util = require("./util")
 const InkProtocol = artifacts.require("./mocks/InkProtocolMock.sol")
 
 contract("InkProtocol", (accounts) => {
-  let protocol
-
   beforeEach(async () => {
-    protocol = await InkProtocol.new()
+    buyer = accounts[1]
+    seller = accounts[2]
+    unknown = accounts[accounts.length - 1]
+    amount = 100
   })
 
   describe("#confirmTransaction()", () => {
@@ -17,7 +18,17 @@ contract("InkProtocol", (accounts) => {
     it("fails when transaction does not exist")
 
     describe("when state is Accepted", () => {
-      it("passes the transaction's amount to the mediator")
+      it("passes the transaction's amount to the mediator", async () => {
+        let { protocol, transaction, mediator } = await $util.buildTransaction(
+          buyer, seller, { finalState: $util.states.Accepted, amount: amount }
+        )
+
+        let tx = await protocol.confirmTransaction(transaction.id, { from: buyer })
+
+        let events = await $util.filterGetSync(mediator.ConfirmTransactionFeeCalled({ transactionAmount: amount }, { fromBlock: 0 }))
+        assert.equal(events.length, 1)
+      })
+
       it("transfers the mediator fee to the mediator")
       it("emits the TransactionConfirmed event")
       it("transfers the tokens to the seller")
